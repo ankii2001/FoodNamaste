e8abf4d497e3088fa3ae20d464afbd2c6cd85588
ankii2001 <ankitluthra2001@gmail.com>
2023-12-30 13:19:40 +0530

    first commit

---
+++ client/src/components/Cancel.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cancel = () => {
+  return (
+    <div className="image">
+      <img className='image-bounce' src='./FailedIMG.png' alt='failed' />
+    </div>
+  )
+}
+
+export default Cancel
---
+++ client/src/components/Sucess.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./style.css";
+
+const Sucess = () => {
+  return (
+    <div className="image">
+      <img className='image-bounce' src='./SuccessIMG.png' alt='success' />
+    </div>
+  )
+}
+
+export default Sucess
